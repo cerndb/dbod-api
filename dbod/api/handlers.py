5001ce53939394323dc749f5cef1534dfa904d63
icoteril <icot@users.noreply.github.com>
2015-04-07 12:25:06 +0200

    APP: Configured application logging

--- dbod/api/handlers.py
+++ dbod/api/handlers.py
@@ -17,18 +17,17 @@ import tornado.web
 import tornado.log
 import base64
 import functools
+import logging
 
-app_log = tornado.log.logging.getLogger("tornado.application")
-app_log.setLevel(tornado.log.logging.DEBUG)
 
 # HTTP API status codes
 NOT_FOUND = 404
 UNAUTHORIZED = 401
 
 # Basic HTTP Authentication decorator
-def http_basic_auth(f):
-    """Decorator for extracting HTTP basic authentication user/password pairs from
-    the request headers and passing them to the decorated method. It will return
+def http_basic_auth(fun):
+    """Decorator for extracting HTTP basic authentication user/password pairs
+    from the request headers and passing them to the decorated method. It will
     generate an HTTP UNAUTHORIZED (Error code 401) if the request is not using
     HTTP basic authentication.
 
@@ -36,8 +35,9 @@ def http_basic_auth(f):
         @http_basic_auth
         def get(self, user, pwd)
     """
-    @functools.wraps(f)
+    @functools.wraps(fun)
     def wrapper(*args, **kwargs):
+        """ Decorator wrapper """
         self = args[0]
         try:
             # Try
@@ -47,13 +47,17 @@ def http_basic_auth(f):
                 # Decode user and password and pass them to the decorated
                 # method
                 user, _, pwd = base64.decodestring(token).partition(':')
-                return f(*args, user=user, pwd=pwd, **kwargs)
+                logging.info("Authentiated access for user %s", user)
+                return fun(*args, user=user, pwd=pwd, **kwargs)
             else:
                 # We only support basic authentication
+                logging.error("Authentication scheme not recognized")
                 return "Authentication scheme not recognized"
-        except AttributeError as e:
+        except AttributeError:
             # Raise UNAUTHORIZED HTTP Error (401) if the request is not
             # using autentication (auth will be None and partition() will fail
+            logging.error("Unauthorized access from: %s",
+                self.request.headers)
             raise tornado.web.HTTPError(UNAUTHORIZED)
 
     return wrapper
@@ -61,6 +65,7 @@ def http_basic_auth(f):
 class DocHandler(tornado.web.RequestHandler):
     """Generates the API endpoint documentation"""
     def get(self):
+        logging.info("Generating API endpoints doc")
         response = """Please use :
             <p>http://hostname:port/api/v1/entity/NAME</p>
             <p>http://hostname:port/api/v1/host/HOSTNAME</p>"""
@@ -70,10 +75,11 @@ class EntityHandler(tornado.web.RequestHandler):
     def get(self, entity):
         """Returns metadata for a certain entity"""
         response = entity_metadata(entity)
-        app_log.debug(response)
         if response:
+            logging.debug(response)
             self.write(response)
         else:
+            logging.warning("Entity not found: %s", entity)
             raise tornado.web.HTTPError(NOT_FOUND)
 
 class HostHandler(tornado.web.RequestHandler):
@@ -82,7 +88,8 @@ class HostHandler(tornado.web.RequestHandler):
             on a certain host"""
         response = host_metadata(host)
         if response:
-            app_log.debug(response)
+            logging.debug(response)
             self.write(response)
         else:
+            logging.warning("Host not found: %s", host)
             raise tornado.web.HTTPError(NOT_FOUND)
