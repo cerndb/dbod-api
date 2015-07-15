b70a7dbc8383c717eb549a6c9cbee5fda95029e3
icoteril <ignacio.coterillo@cern.ch>
2015-07-15 17:24:39 +0200

    TEST: Added stub for db_tests

---
+++ dbod/tests/db_tests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2015, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+import unittest
+
+class TestDB(unittest.TestCase):
+
+    def test_entity_metadata(self):
+        pass
+
+    def test_host_metadata(self):
+        pass
+
+    def test_last_dnsname(self):
+        pass
+
+    def test_next_dnsname(self):
+        pass
+    
+    def test_add_functional_alias(self):
+        pass
+
+    def test_update_functional_alias(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
