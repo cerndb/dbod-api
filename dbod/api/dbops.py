c836aeee86d6aacde142e1bc056183e8868ab839
icoteril <ignacio.coterillo@cern.ch>
2015-07-16 14:45:52 +0200

    DB: Using connection pool and connection and cursors as contexts (with)

--- dbod/api/dbops.py
+++ dbod/api/dbops.py
@@ -12,7 +12,7 @@
 This file contains all database related code
 """
 
-from psycopg2 import connect, DatabaseError
+from psycopg2 import connect, DatabaseError, pool, errorcodes
 import ConfigParser
 import sys, traceback, json, logging
 
@@ -35,118 +35,106 @@ except ConfigParser.NoOptionError:
 #STATUS CODES
 NOT_FOUND = 404
 
+POOL = pool.ThreadedConnectionPool(5, 20,
+        database=db, user=dbuser, host=dbhost, port=port, password=password)
+
 def entity_metadata(entity):
     """Returns a JSON object containing all the metadata for a certain entity"""
     try:
-        conn = connect(database=db, user=dbuser, host=dbhost, port=port, 
-                password=password)
-        cur = conn.cursor()
-        cur.execute("""select data from metadata where name = %s""", (entity, ))
-        res = cur.fetchone()
-        cur.close()
-        if res:
-            return res[0]
-        else:
-            return None
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select data from metadata where name = %s""",
+                        (entity, ))
+                res = curs.fetchone()
+                if res:
+                    return res[0]
+                else:
+                    return None
     except DatabaseError as dberr:
-        # Problem connecting to database, return result from cache?
+        logging.error(errorcodes.lookup(dberr.pgcode[:2]))
+        logging.error(errorcodes.lookup(dberr.pgcode))
         return None
 
 def host_metadata(host):
     """Returns a JSON object containing the metadata for all the entities
         residing on a host"""
     try:
-        conn = connect(database=db, user=dbuser, host=dbhost, port=port,
-                password=password)
-        cur = conn.cursor()
-        cur.execute("""select name, data
-        from (
-            select name, json_array_elements(data->'hosts') host, data
-                from metadata)
-            as foo
-            where trim(foo.host::text, '"') = %s""", (host, ))
-        res = cur.fetchall()
-        cur.close()
-        if res:
-            return json.dumps(res)
-        else:
-            return None
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select name, data
+                from (
+                    select name, json_array_elements(data->'hosts') host, data
+                        from metadata)
+                    as foo
+                    where trim(foo.host::text, '"') = %s""", (host, ))
+                res = curs.fetchall()
+                if res:
+                    return json.dumps(res)
+                else:
+                    return None
     except DatabaseError as dberr:
-        # Problem connecting to database, return result from cache?
+        logging.error(errorcodes.lookup(dberr.pgcode[:2]))
+        logging.error(errorcodes.lookup(dberr.pgcode))
         return None
 
 # Functional aliases related methods
+#   The assumption for the first implementation is that the database
+#   table contains empty entries for pre-created dnsnames that are
+#   considered valid
 
 def next_dnsname():
     """Returns the next dnsname which can be used for a newly created
         instance, if any."""
     try:
-        conn = connect(database=db, user=dbuser, host=dbhost, port=port,
-                password=password)
-        cur = conn.cursor()
-        cur.execute("""select dns_name
-        from functional_aliases
-        where db_name = NULL order by dns_name desc limit 1""")
-        res = cur.fetchOne()
-        cur.close()
-        if res:
-            return json.dumps(res)
-        else:
-            return None
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select dns_name
+                from functional_aliases
+                where db_name is NULL order by dns_name desc limit 1""")
+                res = curs.fetchone()
+                if res:
+                    return json.dumps(res)
+                else:
+                    return None
     except DatabaseError as dberr:
-        # Problem connecting to database, return result from cache?
-        return None
-
-def last_dnsname():
-    """Returns the last used dnsname"""
-    try:
-        conn = connect(database=db, user=dbuser, host=dbhost, port=port,
-                password=password)
-        cur = conn.cursor()
-        cur.execute("""select dns_name
-        from functional_aliases
-        order by dns_name desc limit 1""")
-        res = cur.fetchone()
-        cur.close()
-        return json.dumps(res)
-    except DatabaseError as dberr:
-        # Problem connecting to database, return result from cache?
-        logging.error(dberr)
-        return None
-
-def add_functional_alias(dnsname, db_name, alias):
-    """Creates a dnsname record with its db_name and alias"""
-    try:
-        conn = connect(database=db, user=dbuser, host=dbhost, port=port,
-                password=password)
-        cur = conn.cursor()
-        cur.execute("""insert into functional_aliases(dns_name, db_name, alias)
-        values(%s,%s,%s)""", (dnsname, db_name, alias))
-        res = cur.fetchall()
-        cur.close()
-        if res:
-            return json.dumps(res)
-        else:
-            return None
-    except DatabaseError as dberr:
-        # Problem connecting to database, return result from cache?
+        logging.error(errorcodes.lookup(dberr.pgcode[:2]))
+        logging.error(errorcodes.lookup(dberr.pgcode))
         return None
 
 def update_functional_alias(dnsname, db_name, alias):
     """Updates a dnsname record with its db_name and alias"""
     try:
-        conn = connect(database=db, user=dbuser, host=dbhost, port=port,
-                password=password)
-        cur = conn.cursor()
-        cur.execute("""update functional_aliases
-        set db_name = %s, alias = %s where dnsname = %s""", (db_name, alias, dnsname))
-        res = cur.fetchall()
-        cur.close()
-        if res:
-            return json.dumps(res)
-        else:
-            return None
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""begin""")
+                curs.execute("""update functional_aliases
+                set db_name = %s, alias = %s where dnsname = %s""", 
+                    (db_name, alias, dnsname,))
+                curs.execute("""commit""")
+                res = curs.statusmessage
+                if res:
+                    return json.dumps(res)
+                else:
+                    return None
     except DatabaseError as dberr:
-        # Problem connecting to database, return result from cache?
+        logging.error(errorcodes.lookup(dberr.pgcode[:2]))
+        logging.error(errorcodes.lookup(dberr.pgcode))
         return None
 
+def get_functional_alias(db_name):
+    """Returns the funcional alias and dnsname for a certain database"""
+    try:
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select dns_name, alias
+                from functional_aliases
+                where db_name = %s""", (db_name,))
+                res = curs.fetchone()
+                if res:
+                    return json.dumps(res)
+                else:
+                    return None
+    except DatabaseError as dberr:
+        logging.error(errorcodes.lookup(dberr.pgcode[:2]))
+        logging.error(errorcodes.lookup(dberr.pgcode))
+        return None
