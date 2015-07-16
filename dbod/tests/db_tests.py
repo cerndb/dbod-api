db0b69028a901b3f47b48eecefe89ae1c8b91a20
icoteril <ignacio.coterillo@cern.ch>
2015-07-16 17:14:02 +0200

    TEST: Full assert test for dbops

--- dbod/tests/db_tests.py
+++ dbod/tests/db_tests.py
@@ -10,36 +10,46 @@
 
 import nose
 from types import *
+import json
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 from dbod.api.dbops import *
 
 def test_entity_metadata():
     metadata =  entity_metadata('pinocho')
-    assert type(metadata) is DictType
+    logging.info("Entity metadata for 'pinocho':\n%s", metadata)
+    if metadata:
+        assert type(metadata) is DictType
 
 def test_host_metadata():
     metadata = host_metadata('db-50019')
-    print type(metadata)
-    assert type(metadata) is StringType
+    logging.info(metadata)
+    if metadata:
+        assert type(metadata) is ListType
 
 def test_get_functional_alias():
     falias = get_functional_alias('pinocho')
-    print falias, type(falias)
-    assert type(falias) is StringType
+    logging.info("Functional alias for 'pinocho' %s", falias)
+    if falias:
+        assert type(falias) is StringType
 
 def test_next_dnsname():
     ndnsname = next_dnsname()
-    print ndnsname, type(ndnsname)
+    logging.info("Next free dnsname: %s", ndnsname)
     assert type(ndnsname) is StringType
 
 def test_update_functional_alias():
     dnsname = next_dnsname()
-    res = update_functional_alias(dnsname, 'test', 'dbod-test')
-    print res
-    print get_functional_alias('test')
-    #update_functional_alias(dnsname, None, None)
-    print get_functional_alias('test')
-
-
+    res = get_functional_alias('test')
+    logging.info("Defined functional alias for 'test': %s", res)
+    assert update_functional_alias(dnsname, 'test', 'dbod-test') == True
+    res = get_functional_alias('test')
+    logging.info("Defined functional alias for 'test': %s", res)
+    assert update_functional_alias(dnsname, None, None) == True
+    res = get_functional_alias('test')
+    logging.info("Defined functional alias for 'test': %s", res)
 
 if __name__ == "__main__":
     pass
