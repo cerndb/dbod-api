a75d09096d3d03892fd2d9c8c3f9fd07452bd4a1
jocorder <j.cordero@cern.ch>
2015-11-23 16:21:32 +0100

    New endpoints for instances.

--- dbod/api/instancehandlers.py
+++ dbod/api/instancehandlers.py
@@ -24,14 +24,51 @@ import json
 
 
 class InstanceHandler(tornado.web.RequestHandler):
-    def get(self, dbname):
-        response = get_instance(dbname)
+    """Retrieves instances by a specific field: dbname, host, status"""
+    def get(self, field, value):
+        query = {'dbname': get_instances_by_dbname, 'host': get_instances_by_host, 'status': get_instances_by_status}
+        try:
+            response = query[field](value)
+        except:
+            logging.warning("Invalid field: %s", field)
+            raise tornado.web.HTTPError(NOT_FOUND)
+
         if response:
-            logging.debug(response)
-            json_str = json.dumps( response )
-            logging.debug(json_str)
+            json_str = json.dumps(response)
             self.write(json_str)
         else:
-            logging.warning("Instance not found: %s", dbname)
+            logging.warning("Instance not found: %s", value)
             raise tornado.web.HTTPError(NOT_FOUND)
+            
+class InstanceListAllHandler(tornado.web.RequestHandler):
+    """Retrieves all the instances, or all the instances of the user if username is provided"""
+    def get(self, username=None):
+        if username:
+            response = get_instances_by_username(username)
+            if response:
+                json_str = json.dumps(response)
+                self.write(json_str)
+            else:
+                logging.warning("Username not found: %s", username)
+                raise tornado.web.HTTPError(NOT_FOUND)
+        else:
+            response = get_all_instances()
+            if response:
+                json_str = json.dumps(response)
+                self.write(json_str)
+            else:
+                logging.warning("No instances found")
+                raise tornado.web.HTTPError(NOT_FOUND)
+            
+class InstanceHostHandler(tornado.web.RequestHandler):
+    """Retrieves the list of hosts and the memory of each one"""
+    def get(self):
+        response = get_host_list()
+        if response:
+            json_str = json.dumps(response)
+            self.write(json_str)
+        else:
+            logging.warning("No hosts found: %s", value)
+            raise tornado.web.HTTPError(NOT_FOUND)
+            
 
--- dbod/api/instanceops.py
+++ dbod/api/instanceops.py
@@ -18,7 +18,7 @@ import sys, traceback, logging
 from dbod.api.dbops import *
 from dbod.api.utils import *
 
-def get_instance(dbname):
+def get_instances_by_dbname(dbname):
     """Returns a JSON object containing all the data for a dbname"""
     try:
         dbname = str(dbname)
@@ -37,3 +37,83 @@ def get_instance(dbname):
         return None
     finally:
         POOL.putconn(conn)
+        
+def get_instances_by_host(host):
+    """Returns a JSON object containing all the data for a host"""
+    try:
+        host = str(host)
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select * from instance where host = %s""",
+                        (host, ))
+                rows = curs.fetchall()
+                cols = [i[0] for i in curs.description]
+                if rows:
+                    return create_json_from_result(rows, cols)
+                return None
+    except DatabaseError as dberr:
+        logging.error("PG Error: %s", dberr.pgerror)
+        logging.error("PG Error lookup: %s", errorcodes.lookup(dberr.pgcode))
+        return None
+    finally:
+        POOL.putconn(conn)
+        
+def get_instances_by_status(status):
+    """Returns a JSON object containing all the data for instances with a status"""
+    try:
+        status = str(status)
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select * from instance where status = %s""",
+                        (status, ))
+                rows = curs.fetchall()
+                cols = [i[0] for i in curs.description]
+                if rows:
+                    return create_json_from_result(rows, cols)
+                return None
+    except DatabaseError as dberr:
+        logging.error("PG Error: %s", dberr.pgerror)
+        logging.error("PG Error lookup: %s", errorcodes.lookup(dberr.pgcode))
+        return None
+    finally:
+        POOL.putconn(conn)
+        
+def get_instances_by_username(username):
+    """Returns a JSON object containing all the instances for the user"""
+    try:
+        username = str(username)
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""select * from instance where username = %s""",
+                        (username, ))
+                rows = curs.fetchall()
+                cols = [i[0] for i in curs.description]
+                if rows:
+                    return create_json_from_result(rows, cols)
+                return None
+    except DatabaseError as dberr:
+        logging.error("PG Error: %s", dberr.pgerror)
+        logging.error("PG Error lookup: %s", errorcodes.lookup(dberr.pgcode))
+        return None
+    finally:
+        POOL.putconn(conn)
+
+def get_all_instances():
+    """Returns a JSON object containing all the instances"""
+    try:
+        with POOL.getconn() as conn:
+            with conn.cursor() as curs:
+                curs.execute("""SELECT username, db_name, e_group, category, creation_date, expiry_date, db_type, db_size, no_connections, project, description, version, state, status, master, slave, host 
+                                FROM instance WHERE status = '1'""")
+                rows = curs.fetchall()
+                cols = [i[0] for i in curs.description]
+                if rows:
+                    return create_json_from_result(rows, cols)
+                return None
+    except DatabaseError as dberr:
+        logging.error("PG Error: %s", dberr.pgerror)
+        logging.error("PG Error lookup: %s", errorcodes.lookup(dberr.pgcode))
+        return None
+    finally:
+        POOL.putconn(conn)
+    
