ed1ab677e0f0b13ce508f76326986d9e09caf74a
jocorder <j.cordero@cern.ch>
2016-07-01 11:32:55 +0200

    TEST: Added 5 metadata tests.

--- dbod/tests/metadata_test.py
+++ dbod/tests/metadata_test.py
@@ -29,16 +29,43 @@ class Metadata(unittest.TestCase):
     def tearDown(self):
         pass
         
-    def test_connection(self):
-        response = requests.get("http://localhost:3000", verify=False)
+    def test_single_instance_by_name(self):
+        response = requests.get("http://localhost:5443/api/v1/metadata/entity/dbod01", verify=False)
+        data = response.json()["response"]
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(data), 1)
+        self.assertEquals(data[0]["db_name"], "dbod01")
+        self.assertTrue(data[0]["volumes"] != None)
+        self.assertTrue(data[0]["host"] != None)
         
-    def test_metadata(self):
-        response = requests.get("http://localhost:3000/metadata", verify=False)
+    def test_no_instance_by_name(self):
+        response = requests.get("http://localhost:5443/api/v1/metadata/entity/invalid", verify=False)
+        self.assertEquals(response.status_code, 404)
+        
+    def test_single_instance_by_host(self):
+        response = requests.get("http://localhost:5443/api/v1/metadata/host/host03", verify=False)
+        data = response.json()["response"]
         self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(data), 1)
+        self.assertTrue(data[0]["volumes"] != None)
+        self.assertEquals(data[0]["host"], "host03")
         
-    def test_metadata_has_5_instances(self):
-        response = requests.get("http://localhost:3000/metadata", verify=False)
-        data = response.json()
-        self.assertEquals(len(data), 5)
+    def test_multiple_instances_by_host(self):
+        response = requests.get("http://localhost:5443/api/v1/metadata/host/host01", verify=False)
+        data = response.json()["response"]
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(data), 4)
+        list = []
+        for i in range(4):
+            self.assertEquals(data[i]["host"], "host01")
+            self.assertTrue(data[i]["volumes"] != None)
+            self.assertNotIn(data[i]["db_name"], list)
+            list.append(data[i]["db_name"])
+        self.assertEquals(len(list), 4)
+        
+    def test_no_instance_by_host(self):
+        response = requests.get("http://localhost:5443/api/v1/metadata/host/invalid", verify=False)
+        self.assertEquals(response.status_code, 404)
+    
+        
         
