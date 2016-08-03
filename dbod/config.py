b0c44d7d37e580488f651848a13603816efc118c
jocorder <j.cordero@cern.ch>
2016-08-03 11:45:39 +0200

    CONFIG: Check if all the fields are present in the config file when application starts.

--- dbod/config.py
+++ dbod/config.py
@@ -18,10 +18,33 @@ args, unk = parser.parse_known_args()
 
 config = None
 
+# Dictionary with the required present fields in the config file
+requiredConfig = {'server': ['port'], 
+                  'logging':['path', 'level', 'stderr'], 
+                  'api':['user', 'pass'], 
+                  'postgrest':['rundeck_resources_url', 'host_aliases_url', 'entity_metadata_url', 'instance_url', 'volume_url', 'attribute_url', 'functional_alias_url'], 
+                  'rundeck':['api_run_job', 'api_job_output', 'api_authorization'], 
+                  'rundeck-jobs':['get-snapshots']}
+
 try:
     # Load configuration from file
     config = ConfigParser.ConfigParser()
+    config.add_section('tornado')
+    config.set('tornado', 'debug', 'false')
     config.read(args.config)
+    
+    # Force load of all required fields to avoid errors in runtime
+    for section, options in requiredConfig.items():
+        for option in options:
+            try:
+                config.get(section, option)
+            except ConfigParser.NoSectionError:
+                print "Section '{0}' not present in config file. Stopping.".format(section)
+                sys.exit(1)
+            except ConfigParser.NoOptionError:
+                print "Option '{0}' not present in section {1} in config file. Stopping.".format(option, section)
+                sys.exit(1)
+    
 except IOError as e:
     traceback.print_exc(file=sys.stdout)
     sys.exit(e.code)
