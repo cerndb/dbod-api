b802a84077b28c7bdff14d0c27a6a9704b319bff
jforjohn <yianni.sotiro@cern.ch>
2016-08-24 10:32:20 +0200

    ALIAS: fixing the tests after refactoring/changing the code

--- dbod/tests/functionalalias_test.py
+++ dbod/tests/functionalalias_test.py
@@ -11,9 +11,9 @@
 
 import json
 import unittest
+import base64
 import requests
 import tornado.web
-import base64
 
 from mock import patch
 from mock import MagicMock
@@ -30,14 +30,17 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     #           'Accept': 'text/json'}
     db_name_test = "dbod_42"
     alias_test = "dbod-dbod-42.cern.ch"
-    authentication = "basic " + base64.b64encode(config.get('api','user') + ":" + config.get('api','pass'))
-
+    authentication = "basic " + \
+                     base64.b64encode(config.get('api', 'user') + \
+                     ":" + config.get('api', 'pass'))
+    
     def get_app(self):
         return tornado.web.Application(handlers)
 
     @timeout(5)
     def test_get_single_alias_by_name(self):
         """test for getting the right data"""
+        print "test_get_single_alias_by_name"
         db_name = 'dbod_01'
         response = self.fetch("/api/v1/instance/alias/%s" %(db_name))
         data = json.loads(response.body)["response"]
@@ -51,6 +54,7 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     @timeout(5)
     def test_get_invalid_dbname(self):
         """test when the db_name does not exist"""
+        print "test_get_invalid_dbname"
         response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test))
         data = response.body
         self.assertEquals(response.code, 404)
@@ -61,6 +65,7 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     @patch('dbod.api.functionalalias.requests.get')
     def test_get_bad_response(self, mock_get):
         """test when the get response code is not 200. Server/api error"""
+        print "test_get_bad_response"
         status_code_test = 503
         mock_get.return_value = MagicMock(spec=requests.models.Response, 
                                           ok=False,
@@ -80,12 +85,13 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     @timeout(5)
     def test_post_valid_request(self):
         """test when the arguments are valid and dns_name is available"""
-        body = 'functional_alias={"%s": "%s"}' %(self.db_name_test, self.alias_test)
-        response = self.fetch("/api/v1/instance/alias/", 
+        print "test_post_valid_request"
+        body = 'alias=' + self.alias_test
+        response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)  
-        self.assertEquals(response.code, 200)
+        self.assertEquals(response.code, 201)
         # confirm you get back the right data
         response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test))
         data = json.loads(response.body)["response"]
@@ -94,13 +100,15 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
         self.assertTrue(data[0]["dns_name"] != None)
         # delete what has been created
         self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
+                   headers={'Authorization': self.authentication},
                    method="DELETE")
 
     @timeout(5)
     def test_post_duplicate(self):
         """test when there is a request to insert a db_name which already exists"""
-        body = 'functional_alias={"dbod_01": "dbod-dbod-01.cern.ch"}'
-        response = self.fetch("/api/v1/instance/alias/", 
+        print "test_post_duplicate"
+        body = 'alias=dbod-dbod-01.cern.ch'
+        response = self.fetch("/api/v1/instance/alias/dbod_01", 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)
@@ -109,58 +117,63 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     @timeout(5)
     def test_post_no_dns(self):
         """test when there are no any dns available"""
-        body = 'functional_alias={"%s": "%s"}' %(self.db_name_test, self.alias_test)
-        self.fetch("/api/v1/instance/alias/", 
+        print "test_post_no_dns"
+        body = 'alias=' + self.alias_test
+        self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                    method="POST", 
                    headers={'Authorization': self.authentication},
                    body=body)
-        body = 'functional_alias={"%s": "%s"}' %(self.db_name_test, self.alias_test)
-        response = self.fetch("/api/v1/instance/alias/", 
+        body = 'alias=' + 'dbod-dbod-24.cern.ch'
+        response = self.fetch("/api/v1/instance/alias/dbod_24", 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)
-        self.assertEquals(response.code, 400)
+        self.assertEquals(response.code, 503)
         # delete what has been created
         self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                    headers={'Authorization': self.authentication},
                    method="DELETE")
 
+
     @timeout(5)
     def test_post_no_valid_argument(self):
-        """test if the provided argument is valid"""
-        body = 'something={"%s": "%s"}' %(self.db_name_test, self.alias_test)
-        response = self.fetch("/api/v1/instance/alias/", 
+        """test if the provided argument is not valid"""
+        print "test_post_no_valid_argument"
+        body = 'something=%s' + self.alias_test
+        response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)
-        self.assertEquals(response.code, 404)
+        self.assertEquals(response.code, 400)
 
     @timeout(5)
     def test_post_bad_argument(self):
-        """test if the provided argument is valid"""
-        body = 'functional_alias={%s: %s}' %(self.db_name_test, self.alias_test)
-        response = self.fetch("/api/v1/instance/alias/", 
+        """test if the provided value of the argument is not valid"""
+        print "test_post_bad_argument"
+        body = 'alias ' + self.alias_test
+        response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)
-        self.assertEquals(response.code, 404)
+        self.assertEquals(response.code, 400)
     
     @timeout(5)
     @patch('dbod.api.functionalalias.requests.get')
     def test_post_nextdns_failure(self, mock_get):
         """test when there is a server error when getting an available dns_name"""
+        print "test_post_nextdns_failure"
         status_code_test = 503
         mock_get.return_value = MagicMock(spec=requests.models.Response, 
                                           ok=False,
                                           status_code=status_code_test)
         
-        body = 'functional_alias={"%s": "%s"}' %(self.db_name_test, self.alias_test)
-        response = self.fetch("/api/v1/instance/alias/", 
+        body = 'alias=%s' + self.alias_test
+        response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)  
         # tornado will still raise a Bad Request -- to be improved 
-        self.assertEquals(response.code, 400)
+        self.assertEquals(response.code, 503)
         # delete what has maybe been created
         self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                    headers={'Authorization': self.authentication},
@@ -169,20 +182,22 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     @timeout(5)
     def test_delete_valid_request(self):
         """test when there is a valid request to delete a previous inserted db_name"""
+        print "test_delete_valid_request"
         # create entity to be deleted
-        body = 'functional_alias={"%s": "%s"}' %(self.db_name_test, self.alias_test)
-        response = self.fetch("/api/v1/instance/alias/", 
+        body = 'alias=' + self.alias_test
+        response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               method="POST", 
                               headers={'Authorization': self.authentication},
                               body=body)
         response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               headers={'Authorization': self.authentication},
                               method="DELETE")
-        self.assertEquals(response.code, 200)
+        self.assertEquals(response.code, 204)
 
-
+    @timeout(5)
     def test_delete_invalid_dbname(self):
         """test when the given db_name to be deleted does not exist"""
+        print "test_delete_invalid_dbname"
         response = self.fetch("/api/v1/instance/alias/%s" %(self.db_name_test), 
                               headers={'Authorization': self.authentication},
                               method="DELETE")
@@ -192,6 +207,7 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
     @patch('dbod.api.functionalalias.requests.get')
     def test_delete_getdns_failure(self, mock_get):
         """test an unsuccessful get of the dns_name"""
+        print "test_delete_getdns_failure"
         status_code_test = 503
         mock_get.return_value = MagicMock(spec=requests.models.Response,
                                           ok=False,
@@ -200,18 +216,18 @@ class FunctionalAliasTest(AsyncHTTPTestCase, unittest.TestCase):
                               headers={'Authorization': self.authentication},
                               method="DELETE")
         # tornado will still give Bad Request error message -- to be improved
-        self.assertEquals(response.code, 400)
+        self.assertEquals(response.code, 503)
 
     @timeout(5)
     @patch('dbod.api.functionalalias.requests.patch')
-    def test_delete_nosuccess_delete(self, mock_get):
+    def test_delete_nosuccess(self, mock_patch):
         """test an unsuccessful deletion"""
+        print "test_delete_nosuccess_delete"
         status_code_test = 503
-        mock_get.return_value = MagicMock(spec=requests.models.Response,
-                                          ok=False,
-                                          status_code=status_code_test) 
+        mock_patch.return_value = MagicMock(spec=requests.models.Response,
+                                            ok=False,
+                                            status_code=status_code_test) 
         response = self.fetch("/api/v1/instance/alias/%s" %('dbod_01'),
                               headers={'Authorization': self.authentication},
                               method="DELETE")
         self.assertEquals(response.code, status_code_test)
-
