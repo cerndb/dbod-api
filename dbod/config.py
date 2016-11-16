b2b6d70b79683e26fc9f792bb913892df5dc4517
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2016-11-16 12:57:56 +0100

    CONFIG: Refactor to facilitate testing
    Slightly refactors the config module to put the body of the module,
    which was being executed at import time inside a subroutine.
    This allow external calls to test different configuration conditions.
    
    Fixes #77

--- dbod/config.py
+++ dbod/config.py
@@ -16,8 +16,6 @@ import ConfigParser
 import sys, traceback
 import argparse
 
-config = None
-
 # Dictionary with the required present fields in the config file
 requiredConfig = {
         'server' : [ 'port' ] ,
@@ -26,7 +24,7 @@ requiredConfig = {
         'postgrest': [ 
             'rundeck_resources_url', 
             'host_aliases_url', 
-            'entity_metadata_url', 
+            'metadata_url', 
             'instance_url', 
             'volume_url', 
             'attribute_url', 
@@ -45,35 +43,50 @@ optionalConfig = {
                       ]
                   }
 
-try:
-    # Load configuration from file
-    config = ConfigParser.ConfigParser()
-    config.add_section('tornado')
-    config.set('tornado', 'debug', 'false')
-    number_read_files = config.read(args.config)
 
-    # check the config file exist and can be read
-    if len(number_read_files) != 1:
-        print "Configuration file '{0}' cannot be read or does not exist. Stopping.".format(args.config)
-        sys.exit(1)
-    
-    # Force load of all required fields to avoid errors in runtime
-    for section, options in requiredConfig.items():
-        for option in options:
-            try:
-                config.get(section, option)
-            except ConfigParser.NoSectionError:
-                print "Section '{0}' not present in config file. Stopping.".format(section)
-                sys.exit(1)
-            except ConfigParser.NoOptionError:
-                print "Option '{0}' not present in section {1} in config file. Stopping.".format(option, section)
-                sys.exit(1)
-    
-except IOError as e:
-    traceback.print_exc(file=sys.stdout)
-    sys.exit(e.code)
-except ConfigParser.NoOptionError:
-    traceback.print_exc(file=sys.stdout)
-    sys.exit(1)
+def load( config_file = None ):
+    """
+    Reads configuration file
+    """
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+                "-c", "--config", default="/etc/dbod/api.cfg", 
+                help = "specify the location of the config file"
+                )
+        args, unk = parser.parse_known_args()
 
+        # Load configuration from file
+        config = ConfigParser.ConfigParser()
+        config.add_section('tornado')
+        config.set('tornado', 'debug', 'false')
+        if config_file:
+            number_read_files = config.read(config_file)
+        else:
+            number_read_files = config.read(args.config)
+
+        # check the config file exist and can be read
+        if len(number_read_files) != 1:
+            print "Configuration file '{0}' cannot be read or does not exist. Stopping.".format(args.config)
+            sys.exit(1)
+        
+        # Force load of all required fields to avoid errors in runtime
+        for section, options in requiredConfig.items():
+            for option in options:
+                try:
+                    config.get(section, option)
+                except ConfigParser.NoSectionError:
+                    print "Section '{0}' not present in config file. Stopping.".format(section)
+                    sys.exit(2)
+                except ConfigParser.NoOptionError:
+                    print "Option '{0}' not present in section {1} in config file. Stopping.".format(option, section)
+                    sys.exit(2)
+        return config
+        
+    except IOError as e:
+        traceback.print_exc(file=sys.stdout)
+        sys.exit(e.code)
+
+# Loads config
+config = load()
 
---
+++ dbod/tests/config_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016, CERN
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".
+# In applying this license, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
+import logging
+import unittest
+import sys
+
+from dbod.config import load
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+class ConfigTest(unittest.TestCase):
+    """Test configuration loading"""
+
+    def test_config_file_not_found(self):
+        with self.assertRaises(SystemExit) as cm:
+            config = load('/path/to/unexisting/file')
+        self.assertEqual(cm.exception.code, 1)
+
+    def test_config_missing_section(self):
+        with self.assertRaises(SystemExit) as cm:
+            config = load('/tmp/api_missing_section.cfg')
+        self.assertEqual(cm.exception.code, 2)
+    
+    def test_config_missing_option(self):
+        with self.assertRaises(SystemExit) as cm:
+            config = load('/tmp/api_missing_option.cfg')
+        self.assertEqual(cm.exception.code, 2)
+
