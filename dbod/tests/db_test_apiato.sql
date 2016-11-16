b9e48a5636891330f07b23f3761027be0c4be7cc
Manuel Martin Marquez <mmartinm@cern.ch>
2016-11-16 10:59:41 +0100

    SQL: Several fixes on apiato schema for backward compatibility

--- dbod/tests/db_test_apiato.sql
+++ dbod/tests/db_test_apiato.sql
@@ -98,6 +98,8 @@ CREATE TABLE apiato.instance (
     creation_date        date NOT NULL,
     expiry_date          date,
     instance_type_id     int NOT NULL,
+    size                 int,
+    no_connections       int,
     project              varchar(128),
     description          varchar(1024),
     version              varchar(128),
@@ -170,14 +172,18 @@ CREATE INDEX job_instance_idx ON apiato.job (instance_id);
 
 -- VOLUME
 CREATE TABLE apiato.volume (
-    volume_id       serial,
-    instance_id     integer NOT NULL,
-    volume_type_id  int NOT NULL,
-    server        varchar(63) NOT NULL,
-    mounting_path varchar(256) NOT NULL,
-    CONSTRAINT volume_pkey           PRIMARY KEY (volume_id),
-    CONSTRAINT volume_instance_fk    FOREIGN KEY (instance_id)    REFERENCES apiato.instance (instance_id),
-    CONSTRAINT volume_volume_type_fk FOREIGN KEY (volume_type_id) REFERENCES apiato.volume_type (volume_type_id)
+  volume_id       serial,
+  instance_id     integer NOT NULL,
+  file_mode       char(4) NOT NULL,
+  owner           varchar(32) NOT NULL,
+  "group"         varchar(32) NOT NULL,
+  server          varchar(63) NOT NULL,
+  mount_options   varchar(256) NOT NULL,
+  mounting_path   varchar(256) NOT NULL,
+  volume_type_id  int NOT NULL,
+  CONSTRAINT volume_pkey           PRIMARY KEY (volume_id),
+  CONSTRAINT volume_instance_fk    FOREIGN KEY (instance_id)    REFERENCES apiato.instance (instance_id),
+  CONSTRAINT volume_volume_type_fk FOREIGN KEY (volume_type_id) REFERENCES apiato.volume_type (volume_type_id)
 );
 CREATE INDEX volume_instance_idx    ON apiato.volume (instance_id);
 CREATE INDEX volume_volume_type_idx ON apiato.volume (volume_type_id);
@@ -406,21 +412,56 @@ CREATE OR REPLACE VIEW apiato_ro.cluster AS
     apiato.get_cluster_instances(apiato.cluster.cluster_id);
 
 
+INSERT INTO apiato.volume_type (type, description)
+    VALUES ('NETAPP', 'NETAPP volume type');
+
 INSERT INTO apiato.instance_type (type, description)
-    VALUES ('zookeeper','zookeeper instance type');
+VALUES ('ZOOKEEPER', 'Zookeeper instance type'),
+       ('MYSQL'    , 'MySQL database type'),
+       ('PG'       , 'PostgreSQL database type');
 
+-- Insert test data for hosts
 INSERT INTO apiato.host (name, memory)
-    VALUES ('host01','2'),
-           ('host02','2');
+VALUES ('host01', 12),
+       ('host02', 24),
+       ('host03', 64),
+       ('host04', 256);
 
-INSERT INTO apiato.cluster (owner, name, category, creation_date, instance_type_id, state, status)
-  VALUES ('zookeeper','cluster01','DEV',CURRENT_TIMESTAMP, 1,'RUNNING','ACTIVE');
+INSERT INTO apiato.cluster (owner, name, e_group, category, creation_date, expiry_date, instance_type_id, project, description, version, master_cluster_id, state, status)
+VALUES ('user05','cluster01','testgroupZ','DEV',now(),NULL,1,'NILE','Test zookeeper cluster 1', '3.4.9',NULL,'RUNNING','ACTIVE');
 
 
-INSERT INTO apiato.instance (owner, name, category, creation_date, instance_type_id, host_id, state, status, cluster_id)
-  VALUES ('zookeeper','node01','DEV',CURRENT_TIMESTAMP, 1, 1,'RUNNING','ACTIVE',1),
-         ('zookeeper','node02','DEV',CURRENT_TIMESTAMP, 1, 1,'RUNNING','ACTIVE',1);
+-- Insert test data for instances
+INSERT INTO apiato.instance (owner, name, e_group, category, creation_date, instance_type_id, size, no_connections, project, description, version, master_instance_id, slave_instance_id, host_id, state, status, cluster_id)
+VALUES ('user01', 'dbod01', 'testgroupA', 'TEST', now(), 2 , 100 , 100 , 'API' , 'Test instance 1'      , '5.6.17', NULL, NULL, 1, 'RUNNING', 'ACTIVE',     NULL),
+       ('user01', 'dbod02', 'testgroupB', 'PROD', now(), 3 , 50  , 500 , 'API' , 'Test instance 2'      , '9.4.4' , NULL, NULL, 3, 'RUNNING', 'ACTIVE',     NULL),
+       ('user02', 'dbod03', 'testgroupB', 'TEST', now(), 2 , 100 , 200 , 'WEB' , 'Expired instance 1'   , '5.5'   , NULL, NULL, 1, 'RUNNING', 'NON-ACTIVE', NULL),
+       ('user03', 'dbod04', 'testgroupA', 'PROD', now(), 3 , 250 , 10  , 'LCC' , 'Test instance 3'      , '9.4.5' , NULL, NULL, 1, 'RUNNING', 'ACTIVE',     NULL),
+       ('user04', 'dbod05', 'testgroupC', 'TEST', now(), 2 , 300 , 200 , 'WEB' , 'Test instance 4'      , '5.6.17', NULL, NULL, 1, 'RUNNING', 'ACTIVE',     NULL),
+       ('user05', 'node01', 'testgroupZ', 'DEV' , now(), 1 , NULL, NULL, 'NILE', 'Test zookeeper inst 1', '3.4.9' , NULL, NULL, 4, 'RUNNING', 'ACTIVE',     1),
+       ('user05', 'node02', 'testgroupZ', 'DEV' , now(), 1 , NULL, NULL, 'NILE', 'Test zookeeper inst 2', '3.4.9' , NULL, NULL, 4, 'RUNNING', 'ACTIVE',     1);
+
+-- Insert test data for volumes
+INSERT INTO apiato.volume (instance_id, volume_type_id, file_mode, owner, "group", server, mount_options, mounting_path)
+VALUES (1, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data1'),
+       (1, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/bin'),
+       (2, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data2'),
+       (4, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard,tcp', '/MNT/data4'),
+       (5, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data5'),
+       (5, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/bin'),
+       (6, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/zk');
+
+-- Insert test data for attributes
+INSERT INTO apiato.instance_attribute (instance_id, name, value)
+VALUES (1, 'port', '5501'),
+       (2, 'port', '6603'),
+       (3, 'port', '5510'),
+       (4, 'port', '6601'),
+       (5, 'port', '5500'),
+       (6, 'port', '2181'),
+       (7, 'port', '2181');
+
 
 INSERT INTO apiato.cluster_attribute (cluster_id, name, value)
-    VALUES (1,'service','service01'),
-           (1,'user','user01');
+    VALUES (1, 'service','zookeeper'),
+           (1, 'user'   ,'zookeeper');
