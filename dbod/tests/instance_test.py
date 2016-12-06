a39e2c2d0bfac3fc6d1e46dca82252c17d765ee1
jocorder <j.cordero@cern.ch>
2016-12-06 11:07:39 +0100

    TESTS: Corrected basic tests for instance endpoint.

--- dbod/tests/instance_test.py
+++ dbod/tests/instance_test.py
@@ -34,34 +34,35 @@ class InstanceTest(AsyncHTTPTestCase):
         """Creation of a new instance in a correct way"""
         instance = """{
         "owner": "testuser", "category": "TEST", "creation_date":"2016-07-20", 
-        "version": "5.6.17", "instance_type_id": 2, "hosts": ["testhost"], "name": "testdb", "state": "MAINTENANCE", "status": "ACTIVE",
+        "version": "5.6.17", "instance_type_id": 2, "host_id": 1, "name": "testdb", "state": "MAINTENANCE", "status": "ACTIVE",
         "volumes": [
             {"group": "ownergroup", "file_mode": "0755", "server": "NAS-server", "mount_options": "rw,bg,hard", "volume_type_id": 1,
             "owner": "TSM", "mounting_path": "/MNT/data1"}, 
             {"group": "ownergroup", "file_mode": "0755", "server": "NAS-server", "mount_options": "rw,bg,hard", "volume_type_id": 1,
             "owner": "TSM", "mounting_path": "/MNT/bin"}
         ],
-        "attributes": {
+        "attributes": [{
             "port": "5505"
-        }}"""
+        }]}"""
         
         # Create the instance
         response = self.fetch("/api/v1/instance/create", method='POST', headers={'Authorization': self.authentication}, body=instance)
         self.assertEquals(response.code, 201)
-        created_id = json.loads(response.body)["insert_instance"]
+        created_id = json.loads(response.body)[0]["insert_instance"]
+        logging.info("Created with ID: " + str(created_id))
         
         # Check the metadata for this new instance
         response = self.fetch("/api/v1/metadata/instance/testdb")
         self.assertEquals(response.code, 200)
         data = json.loads(response.body)["response"]
-        self.assertEquals(data[0]["name"], "testdb")
+        self.assertEquals(data[0]["db_name"], "testdb")
         self.assertEquals(len(data[0]["volumes"]), 2)
         self.assertEquals(len(data[0]["attributes"]), 1)
         self.assertEquals(data[0]["attributes"]["port"], "5505")  # Reminder: the port is saved as a String in DB
         
         # Delete the created instance
-        response = self.fetch("/api/v1/instance/" + created_id, method='DELETE', headers={'Authorization': self.authentication})
-        self.assertEquals(response.code, 204)
+        response = self.fetch("/api/v1/instance/" + str(created_id), method='DELETE', headers={'Authorization': self.authentication})
+        self.assertEquals(response.code, 201)
         
         # Check again, the metadata should be empty
         response = self.fetch("/api/v1/metadata/instance/testdb")
@@ -72,29 +73,30 @@ class InstanceTest(AsyncHTTPTestCase):
         """Creation of an instance with only basic (required) data"""
         instance = """{
         "owner": "testuser", "category": "TEST", "creation_date":"2016-07-20",
-        "version": "5.6.17", "instance_type_id": 2, "db_size": "100", "hosts": ["testhost"], "name": "testdb", "state": "MAINTENANCE", "status": "ACTIVE"
+        "version": "5.6.17", "instance_type_id": 2, "db_size": "100", "host_id": 1, "name": "testdb", "state": "MAINTENANCE", "status": "ACTIVE"
         }"""
 
         # Create the instance
         response = self.fetch("/api/v1/instance/create", method='POST', headers={'Authorization': self.authentication}, body=instance)
         self.assertEquals(response.code, 201)
-        created_id = json.loads(response.body)["insert_instance"]
+        created_id = json.loads(response.body)[0]["insert_instance"]
+        logging.info("Created with ID: " + str(created_id))
 
         # Check the metadata for this new instance
         response = self.fetch("/api/v1/metadata/instance/testdb")
         self.assertEquals(response.code, 200)
         data = json.loads(response.body)["response"]
-        self.assertEquals(data[0]["name"], "testdb")
-        self.assertEquals(data[0]["hosts"][0], "testhost")
-        self.assertEquals(data[0]["owner"], "testuser")
-        self.assertEquals(data[0]["instance_type_id"], 2)
+        self.assertEquals(data[0]["db_name"], "testdb")
+        self.assertEquals(data[0]["hosts"][0], "host01")
+        self.assertEquals(data[0]["username"], "testuser")
+        self.assertEquals(data[0]["type"], "MYSQL")
         self.assertEquals(data[0]["version"], "5.6.17")
         self.assertTrue(not data[0]["volumes"])
         self.assertTrue(not data[0]["attributes"])
 
         # Delete the created instance
-        response = self.fetch("/api/v1/instance/" + created_id, method='DELETE', headers={'Authorization': self.authentication})
-        self.assertEquals(response.code, 204)
+        response = self.fetch("/api/v1/instance/" + str(created_id), method='DELETE', headers={'Authorization': self.authentication})
+        self.assertEquals(response.code, 201)
 
         # Check again, the metadata should be empty
         response = self.fetch("/api/v1/metadata/instance/testdb")
@@ -108,18 +110,18 @@ class InstanceTest(AsyncHTTPTestCase):
         restore = """{"owner": "user01"}"""
 
         # Edit the instance
-        response = self.fetch("/api/v1/instance/dbod01", method='PUT', headers={'Authorization': self.authentication}, body=instance)
-        self.assertEquals(response.code, 204)
+        response = self.fetch("/api/v1/instance/1", method='PUT', headers={'Authorization': self.authentication}, body=instance)
+        self.assertEquals(response.code, 201)
 
         # Check the metadata for this instance
         response = self.fetch("/api/v1/metadata/instance/dbod01")
         self.assertEquals(response.code, 200)
         data = json.loads(response.body)["response"]
-        self.assertEquals(data[0]["owner"], "newuser")
+        self.assertEquals(data[0]["username"], "newuser")
 
         # Restore the instance
-        response = self.fetch("/api/v1/instance/dbod01", method='PUT', headers={'Authorization': self.authentication}, body=restore)
-        self.assertEquals(response.code, 204)
+        response = self.fetch("/api/v1/instance/1", method='PUT', headers={'Authorization': self.authentication}, body=restore)
+        self.assertEquals(response.code, 201)
 
     @timeout(5)
     def test_edit_instance_dbname(self):
@@ -128,17 +130,17 @@ class InstanceTest(AsyncHTTPTestCase):
         restore = """{"name": "dbod01"}"""
 
         # Edit the instance
-        response = self.fetch("/api/v1/instance/dbod01", method='PUT', headers={'Authorization': self.authentication}, body=instance)
-        self.assertEquals(response.code, 204)
+        response = self.fetch("/api/v1/instance/1", method='PUT', headers={'Authorization': self.authentication}, body=instance)
+        self.assertEquals(response.code, 201)
 
         # Check the metadata for this instance
         response = self.fetch("/api/v1/metadata/instance/newdb01")
         self.assertEquals(response.code, 200)
         data = json.loads(response.body)["response"]
-        self.assertEquals(data[0]["name"], "newdb01")
+        self.assertEquals(data[0]["db_name"], "newdb01")
 
         # Restore the instance
-        response = self.fetch("/api/v1/instance/newdb01", method='PUT', headers={'Authorization': self.authentication}, body=restore)
-        self.assertEquals(response.code, 204)
+        response = self.fetch("/api/v1/instance/1", method='PUT', headers={'Authorization': self.authentication}, body=restore)
+        self.assertEquals(response.code, 201)
 
 
