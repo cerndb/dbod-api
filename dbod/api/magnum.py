d5f9dafe5ff3b11e0a13ee9cd38dc24fd4febfe1
isotirop <yianni.sotiro@cern.ch>
2017-02-24 16:08:08 +0100

    MAGNUM: changing variable names and breaking it into simpler pieces

--- dbod/api/magnum.py
+++ dbod/api/magnum.py
@@ -14,148 +14,130 @@ import logging
 import json
 import requests
 import tornado.web
-import tornado.escape
+import tornado.gen
 from dbod.api.base import *
 from dbod.config import config
+import shlex
+from subprocess import check_output, CalledProcessError
+from os import mkdir, path
+import time
+
 
 class MagnumClusters(tornado.web.RequestHandler):
     """This is the handler of **/instance/containers/<name>** endpoint"""
 
-    token_header = 'X-Subject-Token'
-    auth_header = 'X-Auth-Token'
-    url = config.get('openstack', 'magnum_cluster_url')
     headers = {'Content-Type': 'application/json'}
-    acc_base_urls = ['clusters', 'bays', 'clustertemplates', 'baymodels']
+    cloud = config.get('containers-provider', 'cloud')
+    certs_dir = config.get(cloud, 'cluster_certs_dir')
 
-    @keystone_auth
+    @cloud_auth(cloud)
     def get(self, **args):
-        xtoken = {self.auth_header: args.get(self.token_header.replace('-','_'))}
-        logging.debug('Arguments:' + str(self.request.arguments))
+        composed_url = self.magnum_config(args)
 
-        base_url = args.get('class')
-        ident = args.get('name')
-
-        if base_url in self.acc_base_urls:
-            composed_url = self.url + '/' + base_url
-            if ident:
-                composed_url = composed_url + '/' + ident
-            logging.info('Requesting ' + composed_url)
-            response = requests.get(composed_url, headers=xtoken)
-            data = response.json()
-            if response.ok and data:
-                logging.debug("response: " + json.dumps(data))
-                self.write({'response' : data})
-            elif response.ok:
-                logging.warning("Magnum resources don't exist in this endpoint: " + composed_url)
-                raise tornado.web.HTTPError(NOT_FOUND)
-            else:
-                logging.error("Error fetching magnum's resources in this endpoint" + composed_url)
-                raise tornado.web.HTTPError(response.status_code)
+        data, status_code = get_function(composed_url, self.headers)
+        if data:
+            logging.debug("response: " + json.dumps(data))
+            self.write({'response' : data})
         else:
-            logging.error("Unsupported endpoint")
-            raise tornado.web.HTTPError(NOT_FOUND)
+            logging.error("Error fetching magnum's resources in this endpoint: " + composed_url)
+            raise tornado.web.HTTPError(status_code)
 
-
-    @keystone_auth
+    @cloud_auth(cloud)
     @http_basic_auth
     def post(self, **args):
-        xtoken = {self.auth_header: args.get(self.token_header.replace('-','_'))}
-
-        logging.debug('Arguments:' + str(self.request.arguments))
-        base_url = args.get('class')
-        #ident = args.get('name')
-
+        composed_url = self.magnum_config(args)
         try:
             cluster_specs = json.loads(self.request.body)
             logging.debug("Creation parameters: " + json.dumps(cluster_specs))
         except ValueError:
-            logging.error("No JSON object could be decoded")
+            logging.error("No JSON object could be decoded from request body")
             raise tornado.web.HTTPError(BAD_REQUEST)
 
-        self.headers.update(xtoken)
-
-        if base_url in self.acc_base_urls:
-            composed_url = self.url + '/' + base_url
-            check_req = requests.get(composed_url+'/'+cluster_specs["name"], headers=self.headers)
-            if check_req.status_code == 409 or check_req.ok:
-                logging.error("Multiple clusters exist with same name.")
-                self.set_status(CONFLICT)
-            else: 
-                response = requests.post(composed_url, json=cluster_specs, headers=xtoken)
-                if response.ok:
-                    data = response.json()
-                    logging.info("New cluster created with name: %s and UUID: %s" %(cluster_specs["name"], data["uuid"]))
-                    logging.info("Call GET /api/v1/magnum/clusters/%s to check when the creation finishes." %(cluster_specs["name"]))
-                    
-                    self.write({'response' : data})
-                    self.set_status(ACCEPTED)
-                else:
-                    logging.error("Error in creating the magnum cluster: " + response.text)
-                    self.set_status(response.status_code)
+        new_cluster = cluster_specs['name']
+        _, status_code = get_function(composed_url + '/' + new_cluster, self.headers)
+        if status_code == 200 or status_code == 409:
+            logging.error("Multiple clusters exist with same name.")
+            self.set_status(CONFLICT)
         else:
-            logging.error("Unsupported endpoint")
-            raise tornado.web.HTTPError(NOT_FOUND)
-
-
-    @keystone_auth
-    @http_basic_auth
-    def put(self, **args):
-        xtoken = {self.auth_header: args.get(self.token_header.replace('-','_'))}
-
-        logging.debug('Arguments:' + str(self.request.arguments))
-        base_url = args.get('class')
-        ident = args.get('name')
-
-        try:
-            cluster_specs = json.loads(self.request.body)
-            logging.debug("Creation parameters: " + json.dumps(cluster_specs))
-        except ValueError:
-            logging.error("No JSON object could be decoded")
-            raise tornado.web.HTTPError(BAD_REQUEST)
-
-        self.headers.update(xtoken)
-
-        if base_url in self.acc_base_urls and ident:
-            composed_url = self.url + '/' + base_url + '/' + ident
-            response = requests.patch(composed_url, json=[cluster_specs], headers=xtoken)
+            response = requests.post(composed_url, json=cluster_specs, headers=self.headers)
             if response.ok:
+                print self.request.uri
                 data = response.json()
-                logging.info("New cluster specs for %s are being applied." %(ident))
-                logging.info("Call GET /api/v1/magnum/clusters/%s to check when the update finishes." %(ident))
-                
+                logging.info("New cluster created with name: %s and UUID: %s" %(new_cluster, data["uuid"]))
+                logging.info("Call GET %s/%s to check when the creation finishes." %(self.request.uri, new_cluster))
+
                 self.write({'response' : data})
                 self.set_status(ACCEPTED)
             else:
-                logging.error("Error in updating the magnum cluster: " + response.text)
+                logging.error("Error in creating the magnum cluster: " + response.text)
                 self.set_status(response.status_code)
-        else:
-            logging.error("Unsupported endpoint")
-            raise tornado.web.HTTPError(NOT_FOUND)
 
-    @keystone_auth
+
+
+    @cloud_auth(cloud)
+    @http_basic_auth
+    def put(self, **args):
+        composed_url = self.magnum_config(args)
+
+        try:
+            cluster_specs = json.loads(self.request.body)
+            logging.debug("Creation parameters: " + json.dumps(cluster_specs))
+        except ValueError:
+            logging.error("No JSON object could be decoded")
+            raise tornado.web.HTTPError(BAD_REQUEST)
+
+        response = requests.patch(composed_url, json=[cluster_specs], headers=self.headers)
+        if response.ok:
+            data = response.json()
+            logging.info("New cluster specs for %s are being applied." %(composed_url))
+            logging.info("Call GET %s to check when the update finishes." %(composed_url))
+
+            self.write({'response' : data})
+            self.set_status(ACCEPTED)
+        else:
+            logging.error("Error in updating the magnum cluster: " + response.text)
+            self.set_status(response.status_code)
+
+    @cloud_auth(cloud)
     @http_basic_auth
     def delete(self, **args):
-        xtoken = {self.auth_header: args.get(self.token_header.replace('-','_'))}
+        composed_url = self.magnum_config(args)
+        ident = args.get('name')
+        response = requests.delete(composed_url, headers=self.headers)
+        if response.ok:
+            logging.info("Delete cluster %s" %(ident))
+            logging.info("Call GET %s to check when the deletion finishes." %(self.request.uri))
+	    cluster_certs_dir = self.certs_dir + '/' + ident
+	    cmd = "mv " + cluster_certs_dir + ' ' + cluster_certs_dir + ".old"
+	    if path.isdir(cluster_certs_dir):
+		try:
+		    check_output(cmd.split())
+		    logging.debug("Delete(renaming) certs directory: " + cluster_certs_dir)
+		except CalledProcessError as e:
+		    logging.warning("Failed to run the command %s with args %s" %(e.cmd))
+                    logging.warning("Command failed with return code: %s and output: %s" %(e.returncode, e.output))
+
+            self.set_status(NO_CONTENT)
+        else:
+            logging.error("Error in deleting the magnum cluster: " + response.text)
+            self.set_status(response.status_code)
+
+    def magnum_config(self, args):
+        token_header = 'X-Subject-Token'
+        auth_header = 'X-Auth-Token'
+        url = config.get(self.cloud, 'cluster_url')
 
         logging.debug('Arguments:' + str(self.request.arguments))
-        base_url = args.get('class')
-        ident = args.get('name')
 
+        xtoken = {auth_header: args.get(token_header)}
         self.headers.update(xtoken)
-
-        if base_url in self.acc_base_urls and ident:
-            composed_url = self.url + '/' + base_url + '/' + ident
-            response = requests.delete(composed_url, headers=xtoken)
-            if response.ok:
-                logging.info("New cluster specs for %s applied." %(ident))
-                logging.info("Call GET /api/v1/magnum/clusters/%s to check when the deletion finishes." %(ident))
-                
-                self.set_status(NO_CONTENT)
-            else:
-		print response.text
-                logging.error("Error in deleting the magnum cluster: " + response.text)
-                self.set_status(response.status_code)
-        else:
-            logging.error("Unsupported endpoint")
-            raise tornado.web.HTTPError(NOT_FOUND)
+        base_url = args.get('resource')
+        ident = args.get('name')
+        composed_url = url
+        if base_url:
+            composed_url = url + '/' + base_url
+        if ident:
+            composed_url = composed_url + '/' + ident
+        logging.info('Requesting ' + composed_url)
+        return composed_url
 
