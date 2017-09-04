3afa379b80a776c7a7ab77c1c379271e5f10ce2f
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-09-04 12:05:27 +0200

    SQL: Added cluster and attributes. Changed order of Types.

--- sql/0.90-base_schema/db_base_local.sql
+++ sql/0.90-base_schema/db_base_local.sql
@@ -5,11 +5,64 @@
 -- granted to it by virtue of its status as Intergovernmental Organization
 -- or submit itself to any jurisdiction.
 
+CREATE SCHEMA IF NOT EXISTS public;
+
+------------------------------------------
+-- TYPES
+------------------------------------------
+CREATE TYPE public.instance_state AS ENUM (
+  'RUNNING',
+  'MAINTENANCE',
+  'AWATING-APPROVAL',
+  'JOB-PENDING'
+);
+
+CREATE TYPE public.instance_status AS ENUM (
+  'ACTIVE',
+  'NON-ACTIVE'
+);
+
+CREATE TYPE public.instance_category AS ENUM (
+  'PROD',
+  'DEV',
+  'TEST',
+  'QA',
+  'REF'
+);
+
+CREATE TYPE public.job_state AS ENUM (
+  'FINISHED_FAIL',
+  'FINISHED_OK'
+);
+
+-- INSTANCE_TYPE
+CREATE TABLE public.instance_type (
+  id serial,
+  type             varchar(64) UNIQUE NOT NULL,
+  description      varchar(1024),
+  CONSTRAINT instance_type_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO public.instance_type (type, description)
+VALUES ('ZOOKEEPER', 'Zookeeper instance type'),
+       ('MYSQL'    , 'MySQL database type'),
+       ('PG'       , 'PostgreSQL database type');
+     
+-- VOLUME TYPE     
+CREATE TABLE public.volume_type (
+  id serial,
+  type            varchar(64) UNIQUE NOT NULL,
+  description     varchar(1024),
+  CONSTRAINT volume_type_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO public.volume_type (type, description)
+VALUES ('NETAPP', 'NETAPP volume type'),
+       ('CEPTH' , 'CEPTH volume type');
+
 ------------------------------
 -- CREATION OF TABLES
 ------------------------------
-
-CREATE SCHEMA IF NOT EXISTS public;
 
 -- DOD_COMMAND_DEFINITION
 CREATE TABLE public.dod_command_definition (
@@ -69,6 +122,15 @@ CREATE TABLE public.dod_instances (
     CONSTRAINT dod_instances_dbname UNIQUE (db_name)
 );
 
+CREATE TABLE public.instance_attribute (
+  id serial,
+  instance_id integer NOT NULL,
+  name varchar(32) NOT NULL,
+  value varchar(250) NOT NULL,
+  CONSTRAINT attribute_pkey PRIMARY KEY (id),
+  CONSTRAINT attribute_instance_id_name_key UNIQUE (instance_id, name)
+);
+
 -- DOD_JOBS
 CREATE TABLE public.dod_jobs (
     id int NOT NULL,
@@ -100,21 +162,21 @@ CREATE TABLE public.dod_upgrades (
 
 -- FIM TABLE
 CREATE TABLE public.fim_data (
-    internal_id character varying(36) NOT NULL,
-    instance_name character varying(64),
-    description character varying(450),
-    owner_account_type character varying(20),
-    owner_first_name character varying(24),
-    owner_last_name character varying(40),
-    owner_login character varying(64),
-    owner_mail character varying(128),
-    owner_phone1 character varying(5),
-    owner_phone2 character varying(5),
-    owner_portable_phone character varying(7),
-    owner_department character varying(3),
-    owner_group character varying(3),
-    owner_section character varying(3)
-)
+    internal_id varchar(36) NOT NULL,
+    instance_name varchar(64),
+    description varchar(450),
+    owner_account_type varchar(20),
+    owner_first_name varchar(24),
+    owner_last_name varchar(40),
+    owner_login varchar(64),
+    owner_mail varchar(128),
+    owner_phone1 varchar(5),
+    owner_phone2 varchar(5),
+    owner_portable_phone varchar(7),
+    owner_department varchar(3),
+    owner_group varchar(3),
+    owner_section varchar(3)
+);
 
 -- VOLUME
 CREATE TABLE public.volume (
@@ -129,6 +191,19 @@ CREATE TABLE public.volume (
     PRIMARY KEY (id)
 );
 
+-- VOLUME ATTRIBUTE
+CREATE TABLE public.volume_attribute (
+  id serial,
+  volume_id integer NOT NULL,
+  name varchar(32) NOT NULL,
+  value varchar(250) NOT NULL,
+  CONSTRAINT volume_attribute_pkey PRIMARY KEY (id),
+  CONSTRAINT volume_attribute_volume_fk FOREIGN KEY (volume_id)
+      REFERENCES public.volume (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT volume_attribute_volume_id_name_key UNIQUE (volume_id, name)
+);
+
 -- HOST
 CREATE TABLE public.host (
     id serial,
@@ -138,23 +213,51 @@ CREATE TABLE public.host (
     CONSTRAINT name_con UNIQUE (name)
 );
 
--- ATTRIBUTE
-CREATE TABLE public.attribute (
-    id serial,
-    instance_id integer NOT NULL,
-    name varchar(32) NOT NULL,
-    value varchar(250) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE (instance_id, name)
-);
-
 -- FUNCTIONAL ALIASES
 CREATE TABLE public.functional_aliases (
-    dns_name character varying(256) NOT NULL,
-    db_name character varying(8),
-    alias character varying(256),
+    dns_name varchar(256) NOT NULL,
+    db_name varchar(8),
+    alias varchar(256),
     CONSTRAINT functional_aliases_pkey PRIMARY KEY (dns_name),
     CONSTRAINT db_name_con UNIQUE (db_name)
+);
+
+-- CLUSTER
+CREATE TABLE public.cluster (
+  id serial,
+  owner varchar(32) NOT NULL,
+  name varchar(128) NOT NULL,
+  e_group varchar(256),
+  category instance_category NOT NULL,
+  creation_date date NOT NULL,
+  expiry_date date,
+  type_id integer NOT NULL,
+  project varchar(128),
+  description varchar(1024),
+  version varchar(128),
+  master_id integer,
+  state instance_state NOT NULL,
+  status instance_status NOT NULL,
+  CONSTRAINT cluster_pkey PRIMARY KEY (id),
+  CONSTRAINT cluster_instance_type_fk FOREIGN KEY (type_id)
+      REFERENCES public.instance_type (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT cluster_master_fk FOREIGN KEY (master_id)
+      REFERENCES public.cluster (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT cluster_name_key UNIQUE (name)
+);
+
+CREATE TABLE public.cluster_attribute (
+  id serial,
+  cluster_id integer NOT NULL,
+  name varchar(32) NOT NULL,
+  value varchar(250) NOT NULL,
+  CONSTRAINT cluster_attribute_pkey PRIMARY KEY (id),
+  CONSTRAINT cluster_attribute_cluster_fk FOREIGN KEY (cluster_id)
+      REFERENCES public.cluster (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE,
+  CONSTRAINT cluster_attribute_cluster_id_name_key UNIQUE (cluster_id, name)
 );
 
 -- Job stats view
@@ -167,43 +270,3 @@ CREATE OR REPLACE VIEW public.command_stats AS
 SELECT command_name, COUNT(*) AS COUNT, ROUND(AVG(completion_date - creation_date) * 24*60*60) AS mean_duration
 FROM dod_jobs GROUP BY command_name;
 
-------------------------------------------
--- TYPES
-------------------------------------------
-CREATE TYPE public.instance_state AS ENUM (
-  'RUNNING',
-  'MAINTENANCE',
-  'AWATING-APPROVAL',
-  'JOB-PENDING'
-);
-
-CREATE TYPE public.instance_status AS ENUM (
-  'ACTIVE',
-  'NON-ACTIVE'
-);
-
-CREATE TYPE public.instance_category AS ENUM (
-  'PROD',
-  'DEV',
-  'TEST',
-  'QA',
-  'REF'
-);
-
-CREATE TYPE public.job_state AS ENUM (
-  'FINISHED_FAIL',
-  'FINISHED_OK'
-);
-
---INSTANCE_TYPE
-CREATE TABLE public.instance_type (
-  id serial,
-  type             varchar(64) UNIQUE NOT NULL,
-  description      varchar(1024),
-  CONSTRAINT instance_type_pkey PRIMARY KEY (id)
-);
-
-INSERT INTO public.instance_type (type, description)
-VALUES ('ZOOKEEPER', 'Zookeeper instance type'),
-       ('MYSQL'    , 'MySQL database type'),
-       ('PG'       , 'PostgreSQL database type');
