303e306caf3bfb294b1696fc4449cbee8bde2de6
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-09-06 17:15:35 +0200

    SQL: Delete all the .sql files for testing.

--- dbod/tests/db_apiato.sql
+++
@@ -1,5 +0,0 @@
-\ir db_apiato_schemas.sql
-\ir db_apiato_tables.sql
-\ir db_apiato_procedures.sql
-\ir db_apiato_views.sql
-\ir db_apiato_insert_values.sql
--- dbod/tests/db_test.sql
+++
@@ -1,7 +0,0 @@
-\ir db_dbod_base.sql
-\ir db_update_instance.sql
-\ir db_cluster.sql
-\ir db_host.sql
-\ir db_auth.sql
-\ir db_instance_auth.sql
-
