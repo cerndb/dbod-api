79a6f1f5ae6154b73cad0f758132ab18ffd1e9e7
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-09-08 10:59:54 +0200

    SQL: Added IDs to avoid having differences.

--- dbod/tests/db_test.sql
+++ dbod/tests/db_test.sql
@@ -1,31 +1,52 @@
+-- Copyright (C) 2015, CERN
+-- This software is distributed under the terms of the GNU General Public
+-- Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".
+-- In applying this license, CERN does not waive the privileges and immunities
+-- granted to it by virtue of its status as Intergovernmental Organization
+-- or submit itself to any jurisdiction.
+
+------------------------------
+-- DATA TO RUN TESTS
+------------------------------
+
+-- Insert types
+INSERT INTO public.volume_type (id, type, description)
+VALUES (1, 'NETAPP', 'NETAPP volume type'),
+       (2, 'CEPTH',  'CEPTH volume type');
+ALTER SEQUENCE volume_type_id_seq RESTART WITH 3;
+
+INSERT INTO public.instance_type (id, type, description)
+VALUES (1, 'ZOOKEEPER', 'Zookeeper instance type'),
+       (2, 'MYSQL'    , 'MySQL database type'),
+       (3, 'PG'       , 'PostgreSQL database type');
+ALTER SEQUENCE instance_type_id_seq RESTART WITH 4;
 
 -- Insert test data for volumes
-INSERT INTO public.volume_type (type, description)
-VALUES ('NETAPP', 'NETAPP volume type'),
-       ('CEPTH',  'CEPTH volume type');
+INSERT INTO public.volume (id, instance_id, volume_type_id, file_mode, owner, "group", server, mount_options, mounting_path)
+VALUES (1, 1, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data1'),
+       (2, 1, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/bin'),
+       (3, 2, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data2'),
+       (4, 4, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard,tcp', '/MNT/data4'),
+       (5, 5, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data5'),
+       (6, 5, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/bin'),
+       (7, 6, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/zk'),
+       (8, 7, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/zk'),
+       (9, 7, 2, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data01');
+ALTER SEQUENCE volume_id_seq RESTART WITH 10;
 
-INSERT INTO public.volume (instance_id, volume_type_id, file_mode, owner, "group", server, mount_options, mounting_path)
-VALUES (1, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data1'),
-       (1, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/bin'),
-       (2, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data2'),
-       (4, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard,tcp', '/MNT/data4'),
-       (5, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data5'),
-       (5, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/bin'),
-       (6, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/zk'),
-       (7, 1, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw', '/MNT/zk'),
-       (7, 2, '0755', 'TSM', 'ownergroup', 'NAS-server', 'rw,bg,hard', '/MNT/data01');
+INSERT INTO public.volume_attribute (id, volume_id, name, value)
+VALUES (1, 8, 'ro', 'TRUE'),
+       (2, 8, 'fw', 'TRUE'),
+       (3, 9, 'ro', 'FALSE');
+ALTER SEQUENCE volume_attribute_id_seq RESTART WITH 4;
 
-INSERT INTO public.volume_attribute (volume_id, name, value)
-VALUES (8, 'ro', 'TRUE'),
-       (8, 'fw', 'TRUE'),
-       (9, 'ro', 'FALSE');
-       
 -- Insert test data for hosts
-INSERT INTO public.host (name, memory)
-VALUES ('host01', 12),
-       ('host02', 24),
-       ('host03', 64),
-       ('host04', 256);
+INSERT INTO public.host (id, name, memory)
+VALUES (1, 'host01', 12),
+       (2, 'host02', 24),
+       (3, 'host03', 64),
+       (4, 'host04', 256);
+ALTER SEQUENCE host_id_seq RESTART WITH 5;
 
 -- Insert test data for instances
 INSERT INTO public.instance (id, owner, name, e_group, category, creation_date, type_id, size, no_connections, project, description, version, master_id, slave_id, host_id, state, status, cluster_id)
@@ -37,6 +58,7 @@ VALUES (1, 'user01', 'dbod01', 'testgroupA', 'TEST', now(), 2 , 100 , 100 , 'API
        (6, 'user04', 'dbod06', 'testgroupC', 'TEST', now(), 2 , 300 , 200 , 'WEB' , 'Test instance 4'      , '5.6.17', NULL, NULL, 1, 'RUNNING', 'ACTIVE',     NULL),
        (7, 'user05', 'node01', 'testgroupZ', 'DEV' , now(), 1 , NULL, NULL, 'NILE', 'Test zookeeper inst 1', '3.4.9' , NULL, NULL, 4, 'RUNNING', 'ACTIVE',     1),
        (8, 'user05', 'node02', 'testgroupZ', 'DEV' , now(), 1 , NULL, NULL, 'NILE', 'Test zookeeper inst 2', '3.4.9' , NULL, NULL, 4, 'RUNNING', 'ACTIVE',     1);
+ALTER SEQUENCE instance_id_seq RESTART WITH 9;
 
 -- Insert test data for attributes
 INSERT INTO public.instance_attribute (instance_id, name, value)
@@ -45,6 +67,7 @@ VALUES (1, 'port', '5501'),
        (3, 'port', '5510'),
        (4, 'port', '6601'),
        (5, 'port', '5500');
+ALTER SEQUENCE instance_attribute_id_seq RESTART WITH 6;
 
 -- Insert test data for database aliases
 INSERT INTO public.functional_aliases (dns_name, db_name, alias)
@@ -55,14 +78,16 @@ VALUES ('db-dbod-dns01','dbod01','dbod-dbod-01.cern.ch'),
        ('db-dbod-dns05', NULL, NULL);
 
 -- Insert test data from clusters
-INSERT INTO public.cluster (owner, name, e_group, category, creation_date, expiry_date, type_id, project, description, version, master_id, state, status)
-VALUES ('user05','cluster01','testgroupZ','DEV',now(),NULL,1,'NILE','Test zookeeper cluster 1', '3.4.9',NULL,'RUNNING','ACTIVE');
+INSERT INTO public.cluster (id, owner, name, e_group, category, creation_date, expiry_date, type_id, project, description, version, master_id, state, status)
+VALUES (1, 'user05','cluster01','testgroupZ','DEV',now(),NULL,1,'NILE','Test zookeeper cluster 1', '3.4.9',NULL,'RUNNING','ACTIVE');
+ALTER SEQUENCE cluster_id_seq RESTART WITH 2;
 
 -- Insert test data for cluster attributes
-INSERT INTO public.cluster_attribute (cluster_id, name, value)
-VALUES (1, 'service','zookeeper'),
-       (1, 'user'   ,'zookeeper');
-       
+INSERT INTO public.cluster_attribute (id, cluster_id, name, value)
+VALUES (1, 1, 'service','zookeeper'),
+       (2, 1, 'user'   ,'zookeeper');
+ALTER SEQUENCE cluster_attribute_id_seq RESTART WITH 3;
+
 -- Insert test data for jobs
 INSERT INTO api.job 
 VALUES (1,1,'user01','dbod01','CLEANUP','MYSQL','01-AUG-17','01-AUG-17','user01','2','FINISHED_FAIL','
