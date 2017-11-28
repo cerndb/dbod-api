b54ac5fcd6f360079f8f82986333b6880a36557a
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-11-28 14:13:36 +0100

    Updated travis.yml.

--- sql/0.90qa-base_schema.sql
+++ sql/0.90qa-base_schema.sql
@@ -5,7 +5,7 @@
 -- granted to it by virtue of its status as Intergovernmental Organization
 -- or submit itself to any jurisdiction.
 
-\ir 0.90-base_schema/1-db_base_local.sql
+\ir 0.90-base_schema/1-db-base.sql
 \ir 0.90-base_schema/2-db_api_procedures.sql
 \ir 0.90-base_schema/3-db_api_views.sql
 \ir 0.90-base_schema/4-db_auth.sql
